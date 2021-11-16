e0520b02cf39de00bd7961981f9865fee00af336
mdaak <md.aak3@gmail.com>
2021-11-16 18:46:14 +0600

    image upload

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-const express = require('express')
-const multer = require('multer')
-const app = express()
-const port = 3000
+const express = require("express");
+const multer = require("multer");
+const webp = require("webp-converter");
+const app = express();
+const port = 3000;
 
+app.set("view engine", "ejs");
 
-app.set("view engine",'ejs')
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+  destination: function (req, file, callback) {
+    callback(null, "uploads/");
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + Date.now()+file.filename)
-  }
-})
+  filename: function (req, file, callback) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    callback(null, file.fieldname + "-" + Date.now() + file.originalname);
+  },
+});
 
-const upload = multer({ storage: storage }).single("image")
+const upload = multer({ storage: storage }).single("image");
 
-app.post('/',(req,res)=>{
-  upload(req,res,err =>{
-    if(err) throw err
-    console.log(req.file.path)
-  })
-})
+app.post("/", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) throw err;
+    console.log(req.file.path);
+
+    //     const result = webp.cwebp("nodejs_logo.jpg","nodejs_logo.webp","-q 80",logging="-v");
+    // result.then((response) => {
+    //   console.log(response);
+    // });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
